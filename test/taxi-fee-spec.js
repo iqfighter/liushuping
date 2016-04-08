e16e9489ee6c71af0f652ed6fa4311f082eda338
Shuping LIU <lsp.lhh@gmail.com>
2016-04-08 23:33:42 +0800

    test fee charging for within 2 kilometers without waiting time

--- test/taxi-fee-spec.js
+++ test/taxi-fee-spec.js
@@ -1,1 +1,10 @@
 var expect = require('chai').expect;
+var taxiFee = require('../taxi-fee');
+
+describe('taxiFee#charge', function() {
+    it('should charge 6 RMB within 2 kilometers without waiting time', function() {
+        expect(taxiFee.charge(1, 0)).to.equal(6);
+        expect(taxiFee.charge(2, 0)).to.equal(6);
+        expect(taxiFee.charge(0, 0)).to.equal(6);
+    });
+})
