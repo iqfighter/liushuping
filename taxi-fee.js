d63072d139a82ae64471ba884285c3af1ce62810
Shuping LIU <lsp.lhh@gmail.com>
2016-04-09 00:38:05 +0800

    fix case: should throw error if given waiting time is invalid

--- taxi-fee.js
+++ taxi-fee.js
@@ -1,18 +1,19 @@
 function charge(kilometers, waitingMinutes) {
     check(kilometers, waitingMinutes);
-    
+
     var distanceCharging = chargeDistance(kilometers);
     var waitingCharging = chargeWaiting(waitingMinutes);
     return Math.round(distanceCharging + waitingCharging);
 }
 
-function check(kilometers, waitingCharging) {
+function check(kilometers, waitingMinutes) {
     if (!(kilometers >= 0)) throw Error("Invalid charging distance!");
+    if (!(waitingMinutes >= 0)) throw Error("Invalid waiting time!");
 }
 
 function chargeDistance(kilometers) {
-    return Math.max(0, kilometers - 8) * 0.4 + 
-           Math.max(0, kilometers - 2) * 0.8 + 6;
+    return Math.max(0, kilometers - 8) * 0.4 +
+        Math.max(0, kilometers - 2) * 0.8 + 6;
 }
 
 function chargeWaiting(waitingMinutes) {
--- test/taxi-fee-spec.js
+++ test/taxi-fee-spec.js
@@ -8,73 +8,71 @@ describe('taxiFee#charge', function() {
         expect(taxiFee.charge(2, 0)).to.equal(6);
         expect(taxiFee.charge(0, 0)).to.equal(6);
     });
-    
+
     it('should charge 0.25 RMB per waiting minute', function() {
         expect(taxiFee.charge(1, 1)).to.equal(6);
-        expect(taxiFee.charge(1, 2)).to.equal(7);    
+        expect(taxiFee.charge(1, 2)).to.equal(7);
         expect(taxiFee.charge(1.1, 1.1)).to.equal(6);
         expect(taxiFee.charge(2, 2)).to.equal(7);
         expect(taxiFee.charge(0, 1)).to.equal(6);
         expect(taxiFee.charge(0, 3)).to.equal(7);
     });
-    
-   it('should charge 0.8 RMB per kilometer within 8 kilometers', function() {
-       expect(taxiFee.charge(3, 0)).to.equal(7);
-       expect(taxiFee.charge(4, 0)).to.equal(8);
-       expect(taxiFee.charge(2.5, 0)).to.equal(6);
-       expect(taxiFee.charge(2.6, 0)).to.equal(6);
-       expect(taxiFee.charge(2.7, 0)).to.equal(7);
-       expect(taxiFee.charge(3, 1)).to.equal(7);
-       expect(taxiFee.charge(3, 3)).to.equal(8);
-   });
-   
-   it('should charge 1.2 RMB per kilometer for distances over 8 kilometers', function() {
-       expect(taxiFee.charge(9, 0)).to.equal(12);
-       expect(taxiFee.charge(10, 0)).to.equal(13);
-       expect(taxiFee.charge(9.5, 0)).to.equal(13);
-       expect(taxiFee.charge(9, 2)).to.equal(13);
-       expect(taxiFee.charge(100, 2.3)).to.equal(122);
-   });
-   
-   it('should throw error when given negative distance', function() {
-       var msg = 'Invalid charging distance!';
-       expect(taxiFee.charge).to.throw(msg);
-       
-       try {
-           taxiFee.charge(-4, 2);
-       } catch (e) {
-           expect(e.message).to.equal(msg);
-       }
-   });
-   
-   it('should throw error when given non-numeric distance value', function() {
-       var msg = 'Invalid charging distance!';
-       
-       try {
-           taxiFee.charge('abc', 2);
-       } catch (e) {
-           expect(e.message).to.equal(msg);
-       }
-   });
-   
-     it('should throw error when given negative waiting time', function() {
-       var msg = 'Invalid waiting time!';
-       expect(taxiFee.charge).to.throw(msg);
-       
-       try {
-           taxiFee.charge(4, -2);
-       } catch (e) {
-           expect(e.message).to.equal(msg);
-       }
-   });
-   
-   it('should throw error when given non-numeric waiting time', function() {
-       var msg = 'Invalid waiting time!';
-       
-       try {
-           taxiFee.charge(4, 'abc');
-       } catch (e) {
-           expect(e.message).to.equal(msg);
-       }
-   });
+
+    it('should charge 0.8 RMB per kilometer within 8 kilometers', function() {
+        expect(taxiFee.charge(3, 0)).to.equal(7);
+        expect(taxiFee.charge(4, 0)).to.equal(8);
+        expect(taxiFee.charge(2.5, 0)).to.equal(6);
+        expect(taxiFee.charge(2.6, 0)).to.equal(6);
+        expect(taxiFee.charge(2.7, 0)).to.equal(7);
+        expect(taxiFee.charge(3, 1)).to.equal(7);
+        expect(taxiFee.charge(3, 3)).to.equal(8);
+    });
+
+    it('should charge 1.2 RMB per kilometer for distances over 8 kilometers', function() {
+        expect(taxiFee.charge(9, 0)).to.equal(12);
+        expect(taxiFee.charge(10, 0)).to.equal(13);
+        expect(taxiFee.charge(9.5, 0)).to.equal(13);
+        expect(taxiFee.charge(9, 2)).to.equal(13);
+        expect(taxiFee.charge(100, 2.3)).to.equal(122);
+    });
+
+    it('should throw error when given negative distance', function() {
+        var msg = 'Invalid charging distance!';
+
+        try {
+            taxiFee.charge(-4, 2);
+        } catch (e) {
+            expect(e.message).to.equal(msg);
+        }
+    });
+
+    it('should throw error when given non-numeric distance value', function() {
+        var msg = 'Invalid charging distance!';
+
+        try {
+            taxiFee.charge('abc', 2);
+        } catch (e) {
+            expect(e.message).to.equal(msg);
+        }
+    });
+
+    it('should throw error when given negative waiting time', function() {
+        var msg = 'Invalid waiting time!';
+
+        try {
+            taxiFee.charge(4, -2);
+        } catch (e) {
+            expect(e.message).to.equal(msg);
+        }
+    });
+
+    it('should throw error when given non-numeric waiting time', function() {
+        var msg = 'Invalid waiting time!';
+
+        try {
+            taxiFee.charge(4, 'abc');
+        } catch (e) {
+            expect(e.message).to.equal(msg);
+        }
+    });
 });
